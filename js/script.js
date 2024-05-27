84771b6619067734dae2c0a978e7b67f3e7bc670
falsumit <136841294+falsumit@users.noreply.github.com>
2024-05-27 14:40:29 -0400

    Removed event listener from js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+// window.addEventListener('resize', () => {
+//     let vh = window.innerHeight * 0.01;
+//     document.documentElement.style.setProperty('--vh', `${vh}px`);
+// });
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`)
